d9be94c8ae8eb17da042d51f7c52c5bd0a8b5ca5
linnore <junleli@link.cuhk.edu.cn>
2023-04-23 03:22:25 +0800

    without_label

--- utils/CUHKSZ_AcademicGraph.py
+++ utils/CUHKSZ_AcademicGraph.py
@@ -26,6 +26,7 @@ class CUHKSZ_AcademicGraph(InMemoryDataset):
 
     def process(self):
         for raw_path in self.raw_paths:
+            print(raw_path)
             with zipfile.ZipFile(raw_path,"r") as zip_ref:
                 zip_ref.extractall(self.raw_dir)
         
@@ -35,28 +36,28 @@ class CUHKSZ_AcademicGraph(InMemoryDataset):
         # Process index mapping from semantic to our dataset
         raw_paper_info = pd.read_csv(os.path.join(unzip_dir, "Raw_Paper_Info.csv"))
         raw_paper_info.columns = raw_paper_info.columns.str.strip()
-        df_reIndexMapping = pd.DataFrame({"AG_Index":raw_paper_info.index}, index=raw_paper_info.Paper_ID)
-        reIndexMapping = dict(zip(raw_paper_info.Paper_ID, raw_paper_info.index))
+        df_reIndexMapping = pd.DataFrame({"AG_Index":raw_paper_info.index}, index=raw_paper_info["paperId"])
+        reIndexMapping = dict(zip(raw_paper_info["paperId"], raw_paper_info.index))
         df_reIndexMapping.to_csv(os.path.join(self.processed_dir, "IndexMapping.csv") )
 
         # Process edge information [edge_index]
         raw_citations = pd.read_csv(os.path.join(unzip_dir, "Raw_Citations.csv"))
         raw_citations.columns = raw_citations.columns.str.strip()
-        citations = pd.DataFrame({"Paper_ID": raw_citations.Paper_ID.map(reIndexMapping),
-                                "Ref_Paper_ID": raw_citations.Ref_Paper_ID.map(reIndexMapping)})
+        citations = pd.DataFrame({"paperId": raw_citations["paperId"].map(reIndexMapping),
+                                "ref_paperId": raw_citations["ref_paperId"].map(reIndexMapping)})
         citations.to_csv(os.path.join(self.processed_dir, "Citations.csv"), index=None)
         edge_index = citations.values.T
 
         # Process node embeding [x]
         raw_embedding = pd.read_csv(os.path.join(unzip_dir, "Raw_Paper_Embedding.csv"))
-        raw_embedding.Paper_ID = raw_embedding.Paper_ID.map(reIndexMapping)
+        raw_embedding["paperId"] = raw_embedding["paperId"].map(reIndexMapping)
         embedding = raw_embedding.copy()
         embedding.to_csv(os.path.join(self.processed_dir, "Embedding.csv"), index=None)
-        x = embedding.drop("Paper_ID", axis=1).values
+        x = embedding.drop("paperId", axis=1).values
 
         # Process node label mapping and node label[y]
         y = None
-        raw_paper_info.Field_of_Study
+        
 
         # Creat Data [Graph]
         cuhksz_ag = Data(x=x, edge_index=edge_index)
